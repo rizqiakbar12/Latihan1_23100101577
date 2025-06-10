5666e24ccacc100c80e53df51ad5766c8cc1ce4a
rizqiakbar12 <rizqiakbar402@gmail.com>
2025-06-10 23:10:30 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika akbar = new Matematika(5,7);
+        
+        System.out.println("Hasil penjumlahan: "+akbar.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+akbar.setPengurangan());
+        System.out.println("Hasil perkalian: "+akbar.setPerkalian());
+        System.out.println("Hasil pembagian: "+akbar.setPembagian());
+    }
 }
